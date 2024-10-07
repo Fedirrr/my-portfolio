2d14dd9d2d9dd107f3ed464a7b66b3f82841f745
fedyapavlyshynets <fedyapavlyshynets@gmail.com>
2024-10-07 18:50:17 +0200

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,24 +29,25 @@ const Navbar = () => {
 
     return (
         <nav className='fixed top-0 left-0 w-full z-50'>
-            <div
-                className='flex justify-between items-center max-w-6xl mx-auto md:my-2 bg-stone-950/30 p-4 backdrop-blur-lg'>
-                <div className='text-white font-semibold text-lg uppercase'>
-                    <a href="#" onClick={scrollToTop}>Fedir Pavlyshynets</a>
-                </div>
+            <div className='w-full bg-stone-950/30 backdrop-blur-lg'>
+                <div className='flex justify-between items-center max-w-6xl mx-auto p-4'>
+                    <div className='text-white font-semibold text-lg uppercase'>
+                        <a href="#" onClick={scrollToTop}>Fedir Pavlyshynets</a>
+                    </div>
 
-                <div className='hidden md:flex space-x-8'>
-                    {renderLinks}
-                </div>
+                    <div className='hidden md:flex space-x-8'>
+                        {renderLinks}
+                    </div>
 
-                <div className='md:hidden'>
-                    <button
-                        onClick={() => setMenuOpen(!menuOpen)}
-                        className='text-white focus:outline-none'
-                        aria-label={menuOpen ? "Close menu" : "Open menu"}
-                    >
-                        {menuOpen ? <RiCloseFill/> : <RiMenu3Fill/>}
-                    </button>
+                    <div className='md:hidden'>
+                        <button
+                            onClick={() => setMenuOpen(!menuOpen)}
+                            className='text-white focus:outline-none'
+                            aria-label={menuOpen ? "Close menu" : "Open menu"}
+                        >
+                            {menuOpen ? <RiCloseFill/> : <RiMenu3Fill/>}
+                        </button>
+                    </div>
                 </div>
             </div>
 
